c4efdbede92ac14e820e862b2b6c34bb8c0abd5f
imrankhan <imran.khan@marginedge.com>
2023-03-30 01:16:50 +0600

    adding all the components in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,25 @@ import {
 } from "./components";
 
 const App = () => {
-  <><div>helerrr</div>
-   
-    {/* <BrowserRouter> */}
-      {/* <div className="relative z-0 bg-primary">
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
-      </div> */}
-    {/* </BrowserRouter> */}
-  </>;
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
