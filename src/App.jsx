f8914b27856f6b8fd656221846d8a6c3363fad4f
imrankhan <imran.khan@marginedge.com>
2023-04-08 10:56:26 +0600

    modifying App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  return <div>3D Developer Portfolio</div>;
+};
 
-function App() {
-
-  return (
-    <div>
-      3D Developer Portfolio
-    </div>
-  )
-}
-
-export default App
+export default App;
