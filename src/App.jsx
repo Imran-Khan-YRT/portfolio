7858d6163ec712e22aed616d1c84a93d0bcc9309
imrankhan <imran.khan@marginedge.com>
2023-04-09 09:51:07 +0600

    find the fix for tailwind working with vite & adding in navbar and app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        {" "}
+        {/*To-do*/}
+        <div>
+          {" "}
+          {/*To-do*/}
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div>
+          {/*To-do*/}
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [Active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      {/*To-do*/}
+      <div>
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />{" "}
+          {/* Generate logo from logo.com */}
+          <p>
+            Adrian <span className="sm:block hidden">| Javascript Mastery</span>{" "}
+            {/*problem with css*/}
+          </p>
+        </Link>
+        {/* <p className="text-red-500">asdsa</p> */}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
