98a735c16d452e5f8cf32e9317766d5ec2a74fae
imrankhan <imran.khan@marginedge.com>
2023-04-15 21:28:08 +0600

    adding Tech secion with 3d balls

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,9 @@ const App = () => {
         <About />
         
         <Experience />
-        {/* 
+        
         <Tech />
+        {/* 
         <Works /> */}
         {/* <Feedbacks /> */}
         {/* <div> */}
