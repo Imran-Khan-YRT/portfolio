9176a6542fe1dc393f4afda33f86de9032748e62
imrankhan <imran.khan@marginedge.com>
2023-04-15 16:08:39 +0600

    complete about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-// import { Tilt } from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({index, title, icon}) => {
-  return <p>{title}</p>;
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='w-[250px]'>
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} 
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="flex flex-col items-center rounded-[20px] min-h-[280px] justify-evenly bg-tertiary py-5 px-12 border-2"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-center text-20px text-white font-bold">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+
+
+    
+  );
 };
 
 const About = () => {
@@ -28,9 +44,9 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => {
-          <ServiceCard key={service.title} index={index} {...service} />;
-        })}
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   );
