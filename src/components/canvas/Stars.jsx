975f53214fa137cad86726ed6a94ad1af56362f9
imrankhan <imran.khan@marginedge.com>
2023-04-19 02:07:06 +0600

    adding the stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -3,12 +3,37 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import React, { Suspense, useRef, useState } from "react";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarsCanvas = () => {
-  return <div></div>;
-};
 
 const Stars = (props) => {
-  return <div>Stars</div>;
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  console.log(sphere);
+  return (
+    
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} position={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.02}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-export default Stars;
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
