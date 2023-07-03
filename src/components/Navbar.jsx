a578d4e7c331a5f6eea63a95ab13e777541c291e
imrankhan <imran.khan@marginedge.com>
2023-07-04 02:38:30 +0600

    change ik_logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src="./ik_logo.png" alt="logo" className="w-9 h-9 object-contain" />
           <p className="flex text-[18px] items-center justify-center">
             Imran &nbsp;
             <span className="sm:block hidden h-full">
