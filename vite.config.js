c138697a7cb9a7d3e1f100121bea57063e0dc494
imrankhan <imran.khan@marginedge.com>
2023-07-06 01:17:21 +0600

    change the baseurl

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base url of the website
-  base: '/3D_PORTFOLIO',
+  base: '/',
   plugins: [react()],
 })
